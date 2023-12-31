28f8a5692aecb4c3fbd820a21fb5014d9393769a
Evan Fleming <evan.gordon.fleming@gmail.com>
2023-12-30 19:20:57 -0800

    Add ChatGPT plugin

---
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("chatgpt").setup()
+    end,
+    keys = {
+      { "<leader>ac", "<cmd>ChatGPT<cr>", desc = "Open Chat", mode = { "n" } },
+      {
+        "<leader>ae",
+        "<cmd>ChatGPTEditWithInstructions<cr>",
+        desc = "Edit selected text or whole window",
+        mode = { "n", "v", "x" },
+      },
+      { "<leader>at", "<cmd>ChatGPTRun add_tests<cr>", desc = "Add Tests", mode = { "n", "v", "x" } },
+      { "<leader>ad", "<cmd>ChatGPTRun docstring<cr>", desc = "Generate Docs", mode = { "n", "v", "x" } },
+      { "<leader>ao", "<cmd>ChatGPTRun optimize_code<cr>", desc = "Optimize Code", mode = { "n", "v", "x" } },
+      { "<leader>ai", "<cmd>ChatGPTRun explain_code<cr>", desc = "Explain Code", mode = { "n", "v", "x" } },
+    },
+  },
+}
